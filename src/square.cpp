08d69433c24931453bb2a37497c2f09ce0925562
kand1bober <kurnevich.slava@gmail.com>
2026-07-03 02:01:06 +0300

    basics from course repo

---
+++ src/square.cpp
@@ -0,0 +1,26 @@
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include "../include/basic_geometry.hpp"
+
+polygon_t input_triangle() {
+  std::vector<point_t> verts(3);
+  point_t temp;
+  for (int i = 0; i < 3; i++) {
+    std::cin >> temp.x;
+    std::cin >> temp.y;
+    verts[i] = temp;
+    assert(std::cin.good());
+  }
+  polygon_t ret{verts};
+  return ret;
+}
+
+int main() {
+  polygon_t triangle1 = input_triangle();
+  polygon_t triangle2 = input_triangle();
+  polygon_t inter = triangle1.get_poly_intersection(triangle2);
+  std::cout << inter.square() << std::endl;
+}
